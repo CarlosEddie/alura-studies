758b6cd7345316d2b17e700ceb4ef45197b16ac6
Carlos Eduardo <dev.carloseddie@outlook.com>
2023-12-10 10:34:34 -0300

    updated components

--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -1,26 +1,45 @@
 import React from "react";
 import Button from "../Button";
 import style from './Form.module.scss';
+import { ITask } from "../../types/task";
+import { v4 as uuidv4} from "uuid";
 
 
-class Form extends React.Component {
+class Form extends React.Component<{
+    setTasks: React.Dispatch<React.SetStateAction<ITask[]>>
+}> {
+
+    state = { 
+        task: "",
+        time: "00:00"
+    }
+
+    addTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        this.props.setTasks(oldTasks => [...oldTasks, {...this.state, selected: false, completed: false, id: uuidv4()}]);
+        this.setState({
+            task: "",
+            time: "00:00"
+        })
+    }
+
     render() {
         return (
-            <form className={style.newTask}>
+            <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="task">
                         Add a new study
                     </label>
-                    <input type="text" name="task" id="task" placeholder="What do you want to study?" required/>
+                    <input type="text" name="task" id="task" value={this.state.task} onChange={event => this.setState({ ...this.state, task:event.target.value})} placeholder="What do you want to study?" required/>
                 </div>
                 <div className={style.inputContainer}>
                     <label htmlFor="time">
                         Time
                     </label>
-                    <input type="time" step="1" name="time" id="time" min="00:00:00" max="01:30:00" required/>
+                    <input type="time" step="1" name="time" value={this.state.time} onChange={event => this.setState({ ...this.state, time: event.target.value})} id="time" min="00:00:00" max="01:30:00" required pattern="\d{2}:\d{2}"/>
                 </div>
 
-                <Button>
+                <Button type="submit" >
                     Add
                 </Button>
 
--- alura-studies/src/components/List/index.tsx
+++ alura-studies/src/components/List/index.tsx
@@ -1,21 +1,13 @@
-import React from "react";
+import { ITask } from '../../types/task';
 import style from  './List.module.scss';
 import Item from "./item";
 
-function List() {
-    const tasks = [{
-        task: 'React',
-        time: '02:00:00'
-    }, {
-        task: 'JavaScript',
-        time: '01:00:00'
-    }, {
-        task: 'TypeScript',
-        time: '03:00:00'
-    }]
+
+function List({tasks}: {tasks: ITask[]}) {
+    
     return (
         <aside className={style.tasksList}>
-            <h2> day studies </h2>
+            <h2> Daily Studies </h2>
             <ul>
                 {tasks.map((item, index) => (
                     <Item key={index} {...item} />
