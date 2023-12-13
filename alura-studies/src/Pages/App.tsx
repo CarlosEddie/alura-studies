0b010d595e9868dc93f2a164fe69d465b8ffa979
Carlos Eduardo <dev.carloseddie@outlook.com>
2023-12-13 13:38:34 -0300

    created the select and finish task functions

--- alura-studies/src/Pages/App.tsx
+++ alura-studies/src/Pages/App.tsx
@@ -7,11 +7,38 @@ import { ITask } from '../types/task';
 
 function App() {
   const [tasks, setTasks] = useState<ITask[]>([])
+  const [selected, setSelected] = useState<ITask>();
+
+
+  function selectTask(taskSelected: ITask) {
+    setSelected(taskSelected);
+    setTasks(previousTasks => previousTasks.map(task => ({
+      ...task,
+      selected: task.id === taskSelected.id ? true : false
+    })));
+  }
+
+  function finishTask() {
+    if(selected) {
+      setSelected(undefined);
+      setTasks(previousTasks => previousTasks.map(task => {
+        if(task.id === selected.id) {
+          return {
+            ...task,
+            selected: false,
+            completed: true,
+          }
+        }
+        return task;
+      }))
+    }
+  }
+
   return (
     <div className={style.AppStyle}>
       <Form setTasks={setTasks}/>
-      <List tasks={tasks}/>
-      <Stopwatch/>
+      <List tasks={tasks} selectTask={selectTask}/>
+      <Stopwatch selected={selected} finishTask={finishTask}/>
     </div>
   );
 }
